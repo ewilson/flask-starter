77b35f8ecb6c31b3d85731e58e0fd80bfd826d6f
Eric Wilson <wilson.eric.n@gmail.com>
2015-10-25 22:11:22 -0400

    added templates, css, etc

--- flask-starter.py
+++ flask-starter.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    name = request.args.get('name')
+    name = name if name else 'World'
+    return render_template('hello.html', name=name)
+
+
+@app.route('/goodbye/<user>')
+def goodbye(user):
+    return render_template('bye.html', user=user)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
